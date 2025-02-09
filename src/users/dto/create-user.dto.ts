0f48c1e30e5acb33c5d44e3bb889724ebe5c1c33
Le Van Quan <levanquan@Les-MacBook-Pro.local>
2025-02-09 23:08:43 +0700

    36. #22.5 Validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
 export class CreateUserDto {
+    @IsEmail()
+    @IsNotEmpty({
+        message: 'Email không được để trống',
+    })
     email: string;
+
+    @IsNotEmpty({
+        message: 'Password không được để trống'
+    })
     password: string;
     name: string;
     address: string;
