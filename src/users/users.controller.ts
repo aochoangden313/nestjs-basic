88946ebf937de8c95b2045268f71390b137cdc25
Le Van Quan <levanquan@192.168.1.121>
2025-02-11 12:00:08 +0700

    49. #37. Nestjs Guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { TestGuard } from './test.guards';
 
 @Controller('users') // => link: /users
 export class UsersController {
@@ -15,6 +16,7 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
+  @UseGuards(TestGuard)
   @Get()
   findAll() {
     return this.usersService.findAll();
