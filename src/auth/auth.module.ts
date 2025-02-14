7f6e0c3404132b262d5f470f23a552ef0e5c2e2d
Le Van Quan <levanquan@192.168.1.86>
2025-02-14 15:55:54 +0700

    51. #39. Sử dụng JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,27 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './passport/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   //khi nhan vao API /stateless/login de biet chay vao passport thi phai import module passport vào 
   // Nói vs JWT tao muốn đăng nhập bằng Json web token hay mật khẩu, thì phải import Local Strategy
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy]
+  imports: [UsersModule, PassportModule,
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secretOrPrivateKey: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+            expiresIn: configService.get<string>('JWT_EXPIRE'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+
+  ],
+  providers: [AuthService, LocalStrategy],
+  exports: [AuthService]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {}
-    
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService
+    ) { }
+
     //username và pass là 2 tham số thư viện passport sẽ ném về
     // khi sử dụng thư viện passport sẽ tự động chạy vào hàm này
     async validateUser(username: string, pass: string): Promise<any> {
@@ -18,5 +22,14 @@ export class AuthService {
             }
         }
         return null;
-      }
+    }
+
+    async login(user: any) {
+        const payload = { 
+            username: user.email, 
+            sub: user._id };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
+    }
 }
