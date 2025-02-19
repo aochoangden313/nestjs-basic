2b54c7c6a985630655113c34f7299fc29e861d2a
Le Van Quan <levanquan@192.168.1.250>
2025-02-19 14:00:21 +0700

    78. #62. Version APIs

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { TransformInterceptor } from './core/transform.interceptor';
 
@@ -31,6 +31,13 @@ async function bootstrap() {
       "preflightContinue": false,
     }
   );
+
+  // config versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'api/v',
+    defaultVersion: ['1', '2']
+  });
 
   await app.listen(configService.get<string>('PORT'));
 }
