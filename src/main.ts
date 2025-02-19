d4e7df312d812678210e4e310012d99056a59b62
Le Van Quan <levanquan@192.168.1.250>
2025-02-19 11:29:49 +0700

    76. #61.1 Transform Response

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { TransformInterceptor } from './core/transform.interceptor';
+
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -18,6 +20,7 @@ async function bootstrap() {
   app.setViewEngine('ejs');
 
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
 
   //enable cors
   app.enableCors(
