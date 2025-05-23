91a66c83c213f416d60557dfe148aa5616a07a61
quanlv.56 <quanlv.56@gmail.com>
2025-05-23 16:40:25 +0700

    refactor mail module to include job matching logic and update email template for personalized job suggestions

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -2,24 +2,81 @@ import { Controller, Get } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { Public, ResponseMessage } from 'src/decorator/customize';
 import { MailerService } from '@nestjs-modules/mailer';
+import { name } from 'ejs';
+import { InjectModel } from '@nestjs/mongoose';
+import { Subscriber, SubscriberDocument } from 'src/subscribers/schemas/subscriber.schemas';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { Job, JobDocument } from 'src/jobs/schemas/job.schema';
 
 @Controller('mail')
 export class MailController {
   constructor(
     private readonly mailService: MailService,
     private mailerService: MailerService,
+
+    @InjectModel(Subscriber.name)
+    private subscriberModel: SoftDeleteModel<SubscriberDocument>,
+
+    @InjectModel(Job.name)
+    private jobModel: SoftDeleteModel<JobDocument>
   ) { }
 
   @Get()
   @Public()
   @ResponseMessage("Test email")
   async handleTestEmail() {
-    await this.mailerService.sendMail({
-      to: "quanlv.mcc@gmail.com",
-      from: "Support Team <support@example.com>", // override default from
-      subject: 'Welcome to Nice App! Confirm your Email',
-      template: "jobs", // HTML body content
-    });
+
+    const jobs = [
+      {
+        name: "Software Engineer",
+        company: "Google",
+        salary: "10000",
+        skills: ["JavaScript", "React", "Node.js"],
+      },
+      {
+        name: "Node JS Engineer",
+        company: "AWS",
+        salary: "5000",
+        skills: ["AWS", "React", "Node.js"],
+      },
+      {
+        name: "Java Engineer",
+        company: "Java Company",
+        salary: "10000",
+        skills: ["Java", "HTML", "Node.js"],
+      },
+    ];
+
+    const subscribers = await this.subscriberModel.find({});
+    for (const subs of subscribers) {
+      const subsSkills = subs.skills;
+      const jobWithMatchingSkills = await this.jobModel.find({ skills: { $in: subsSkills } });
+
+      console.log("jobWithMatchingSkills", jobWithMatchingSkills);
+      if (jobWithMatchingSkills.length > 0) {
+        const jobs = jobWithMatchingSkills.map(job => {
+          return {
+            name: job.name,
+            company: job.company.name,
+            salary: job.salary.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " đ", // format salary with commas
+            skills: job.skills,
+          }
+        })
+
+        await this.mailerService.sendMail({
+          to: "quanlv.mcc@gmail.com",
+          from: "Support Team <support@example.com>", // override default from
+          subject: 'Welcome to Nice App! Confirm your Email',
+          template: "jobs", // HTML body content
+          context: {
+            receiver: subs.name,
+            jobs: jobs,
+          }
+        });
+      }
+    };
+    // todo
+    // build template
   }
 
 }
--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -5,11 +5,14 @@ import { ConfigService } from '@nestjs/config';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { join } from 'path';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { Subscriber, SubscriberSchema } from 'src/subscribers/schemas/subscriber.schemas';
+import { Job, JobSchema } from 'src/jobs/schemas/job.schema';
 
 // mail.module.ts
 @Module({
   imports: [
+
     MailerModule.forRootAsync({
       useFactory: async (configService: ConfigService) => ({
         transport: {
@@ -27,13 +30,17 @@ import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handleba
             strict: true,
           },
         },
-        preview: true,
+        preview: configService.get<string>('EMAIL_PREVIEW') === 'true' ? true : false,
       }),
-      inject: [ConfigService ],
+      inject: [ConfigService],
     }),
+    MongooseModule.forFeature([
+      { name: Subscriber.name, schema: SubscriberSchema },
+      { name: Job.name, schema: JobSchema }
+    ]),
   ],
   controllers: [MailController],
   providers: [MailService],
 })
 
-export class MailModule {}
+export class MailModule { }
