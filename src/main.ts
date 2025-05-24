70d06152fd3bebe013af737e608999a3bee727d5
quanlv.56 <quanlv.56@gmail.com>
2025-05-24 15:26:34 +0700

    add helmet middleware for enhanced security in main application

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { TransformInterceptor } from './core/transform.interceptor';
 import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -43,6 +44,8 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: ['1', '2']
   });
+
+  app.use(helmet());
 
   await app.listen(configService.get<string>('PORT'));
 }
